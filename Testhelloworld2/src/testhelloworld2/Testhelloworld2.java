5f3c110fd7f2aed6d1d2e94f3f8dd18fc18d70ff
Harish Raghunathan <harish.raghunathan@fire.nsw.gov.au>
2016-12-14 12:31:38 +1100

    testhar2

--- Testhelloworld2/src/testhelloworld2/Testhelloworld2.java
+++ Testhelloworld2/src/testhelloworld2/Testhelloworld2.java
@@ -8,6 +8,6 @@ public class Testhelloworld2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, World");
-	}
+	} // Another test by harish
 
 }
